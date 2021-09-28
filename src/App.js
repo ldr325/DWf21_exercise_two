971ec42281de84235955b80b31a9a5ee012800b3
LeoR <ldr325@nyu.edu>
2021-09-28 09:39:33 -0400

    setting variables and weathercard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import './App.css';
-import Home from './containers/Home';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom"
+import "./App.css";
+import Home from "./containers/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function WeatherCard({
+  cloudiness,
+  currentTemp,
+  highTemp,
+  humidity,
+  lowTemp,
+  weatherType,
+  windSpeed,
+}) {
+  return (
+    <section className="WeatherCard">
+      <p>
+        cloudiness: <strong>{cloudiness}</strong>
+      </p>
+      <p>
+        currentTemp: <strong>{currentTemp}</strong>
+      </p>
+      <p>
+        highTemp: <strong>{highTemp}</strong>
+      </p>
+      <p>
+        humidity: <strong>{humidity}</strong>
+      </p>
+      <p>
+        lowTemp: <strong>{lowTemp}</strong>
+      </p>
+      <p>
+        WeatherType: <strong>{weatherType}</strong>
+      </p>
+      <p>
+        windSpeed: <strong>{windSpeed}</strong>
+      </p>
+    </section>
+  );
+}
+
+export default WeatherCard;
