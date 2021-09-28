218148c02f066cc3f26300a4f892171d758486a4
LeoR <ldr325@nyu.edu>
2021-09-28 08:52:38 -0400

    react browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Home from './containers/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-    hello
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
